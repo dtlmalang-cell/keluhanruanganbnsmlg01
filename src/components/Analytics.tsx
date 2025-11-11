2a173864dc73d27f3954a55d0155e0d77d8ec4a8
dtlmalang-cell <dtl.malang@gmail.com>
2025-11-11 11:54:05 +0700

    Updated Analytics.tsx

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -568,7 +568,7 @@ const exportToCSV = () => {
               <td className="px-6 py-4 whitespace-nowrap text-sm">
                 <span
                   className={`px-3 py-1 text-xs font-medium rounded-full ${
-                    c?.status === 'ontime'
+                    c?.status === 'On Time'
                       ? 'bg-green-100 text-green-800'
                       : 'bg-amber-100 text-amber-800'
                   }`}
